54e73f1d9598e34475ef8a4456c6f9ca58cddadc
jamesfowler-dev <james_fowler21@hotmail.com>
2025-11-25 15:58:39 +0000

    add component model and migrate

--- components/admin.py
+++ components/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Components
 
 # Register your models here.
+admin.site.register(Components)
--- components/models.py
+++ components/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Components(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('CPU', 'CPU'),
+        ('GPU', 'GPU'),
+        ('RAM', 'RAM'),
+        ('SSD', 'SSD'),
+        ('PSU', 'Power Supply'),
+        ('CASE', 'Case'),
+        ('COOLER', 'Cooler'),
+        ('MOTHERBOARD', 'Motherboard'),
+        ('FAN', 'Fan')
+    ]
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, unique=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.name} | {self.price}"
